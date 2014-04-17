ef460f7d8f6e548a9781aa522138f598b73a1405
Colby Natale <dbsmash@gmail.com>
2014-04-17 09:25:29 -0600

    first commit

---
+++ app/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('RecordController', [function() {
+
+  }])
+  .controller('SearchController', [function() {
+
+  }]);
