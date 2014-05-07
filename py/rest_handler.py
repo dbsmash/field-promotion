5a1c28782266d7c8ec1b796493de44a8bf5fed9c
Colby Natale <colby.natale@webfilings.com>
2014-05-07 13:43:44 -0600

    changes to show win loss results in color, as well as some other fixes

--- py/rest_handler.py
+++ py/rest_handler.py
@@ -15,15 +15,17 @@ class GameHandler(webapp2.RequestHandler):
         if not current_user:
             user = User(user_id = cu.user_id(), name = cu.nickname(), email=cu.email())
             user.put()
+        else:
+            current_user.put()
 
     def get(self):
-    	"""
-    	GET ALL games - and filters if there are search criteria
-    	"""
+        """
+        GET ALL games - and filters if there are search criteria
+        """
         self.record_usage()
-    	game_list = []
-    	namespace_manager.set_namespace(users.get_current_user().user_id())
-    	query = Game.query()
+        game_list = []
+        namespace_manager.set_namespace(users.get_current_user().user_id())
+        query = Game.query()
 
         if self.request.get('player_faction'):
             query = query.filter(Game.player_faction == self.request.get('player_faction'))
@@ -45,25 +47,25 @@ class GameHandler(webapp2.RequestHandler):
 
         games = query.fetch(1000)
 
-    	for game in games:
+        for game in games:
             if game.won:
                 win_count = win_count + 1
             if not game.teaching and not game.draw:
                 non_teaching_count = non_teaching_count + 1
-    		game_map = {}
-    		game_map['player_faction'] = game.player_faction
-	    	game_map['player_warcaster'] = game.player_warcaster
-	    	game_map['opponent_name'] = game.opponent_name
-	    	game_map['opponent_faction'] = game.opponent_faction
-	    	game_map['opponent_warcaster'] = game.opponent_warcaster
-	    	game_map['result'] = game.result
-	    	game_map['size'] = game.size
-	    	game_map['won'] = game.won
-	    	game_map['draw'] = game.draw
-	    	game_map['teaching'] = game.teaching
-	    	game_map['date'] = str(game.date)
-	    	game_map['key'] = game.key.urlsafe()
-    		game_list.append(game_map)
+            game_map = {}
+            game_map['player_faction'] = game.player_faction
+            game_map['player_warcaster'] = game.player_warcaster
+            game_map['opponent_name'] = game.opponent_name
+            game_map['opponent_faction'] = game.opponent_faction
+            game_map['opponent_warcaster'] = game.opponent_warcaster
+            game_map['result'] = game.result
+            game_map['size'] = game.size
+            game_map['won'] = game.won
+            game_map['draw'] = game.draw
+            game_map['teaching'] = game.teaching
+            game_map['date'] = str(game.date)
+            game_map['key'] = game.key.urlsafe()
+            game_list.append(game_map)
         game_list.sort(key=lambda game: game['date'], reverse=True)
         results = {
             'games': game_list,
@@ -71,27 +73,27 @@ class GameHandler(webapp2.RequestHandler):
             'non_teaching_count': non_teaching_count
         }
 
-    	self.response.out.write(json.dumps(results))
+        self.response.out.write(json.dumps(results))
 
     def post(self):
-    	"""
-    	CREATE a new Game entity 
-    	"""
+        """
+        CREATE a new Game entity 
+        """
         self.record_usage()
-    	namespace_manager.set_namespace(users.get_current_user().user_id())
-    	param_map = {}
-    	param_map['player_faction'] = self.request.get('player_faction')
-    	param_map['player_warcaster'] = self.request.get('player_warcaster')
-    	param_map['opponent_name'] = self.request.get('opponent_name', '')
-    	param_map['opponent_faction'] = self.request.get('opponent_faction')
-    	param_map['opponent_warcaster'] = self.request.get('opponent_warcaster')
-    	param_map['result'] = self.request.get('result')
-    	param_map['size'] = int(self.request.get('size', 0))
-    	param_map['won'] = self.request.get('won') == 'true'
-    	param_map['draw'] = self.request.get('draw') == 'true'
-    	param_map['teaching'] = self.request.get('teaching') == 'true'
-    	date = self.request.get('date')
-    	try:
+        namespace_manager.set_namespace(users.get_current_user().user_id())
+        param_map = {}
+        param_map['player_faction'] = self.request.get('player_faction')
+        param_map['player_warcaster'] = self.request.get('player_warcaster')
+        param_map['opponent_name'] = self.request.get('opponent_name', '')
+        param_map['opponent_faction'] = self.request.get('opponent_faction')
+        param_map['opponent_warcaster'] = self.request.get('opponent_warcaster')
+        param_map['result'] = self.request.get('result')
+        param_map['size'] = int(self.request.get('size', 0))
+        param_map['won'] = self.request.get('won') == 'true'
+        param_map['draw'] = self.request.get('draw') == 'true'
+        param_map['teaching'] = self.request.get('teaching') == 'true'
+        date = self.request.get('date')
+        try:
             real_date = datetime.strptime(date,'%m/%d/%Y')
         except (ValueError):
             real_date = datetime.strptime(date,'%Y-%m-%d')
@@ -102,56 +104,56 @@ class GameHandler(webapp2.RequestHandler):
         self.response.out.write(game.key.urlsafe())
 
     def put(self):
-    	"""
-    	UPDATE an existing Game entity
-    	"""
+        """
+        UPDATE an existing Game entity
+        """
         self.record_usage()
-    	namespace_manager.set_namespace(users.get_current_user().user_id())
-    	key_str = self.request.get('key')
-    	key = ndb.Key(urlsafe=key_str)
-    	game = key.get()
-    	if not game:
-    		self.response.set_code(500)
-    		return
-    	if self.request.get('player_faction'):
-    		game.player_faction = self.request.get('player_faction')
-    	if self.request.get('player_warcaster'):
-    		game.player_warcaster = self.request.get('player_warcaster')
-    	if self.request.get('opponent_name'):
-    		game.opponent_name = self.request.get('opponent_name')
-    	if self.request.get('opponent_faction'):
-    		game.opponent_faction = self.request.get('opponent_faction')
-    	if self.request.get('opponent_warcaster'):
-    		game.opponent_warcaster = self.request.get('opponent_warcaster')
-    	if self.request.get('result'):
-    		game.result = self.request.get('result')
-    	if self.request.get('size'):
-    		game.size = int(self.request.get('size', 0))
-    	if self.request.get('won'):
-    		game.won = bool(self.request.get('won'))
-    	if self.request.get('draw'):
-    		game.draw = bool(self.request.get('draw'))
-    	if self.request.get('teaching'):
-    		game.teaching = bool(self.request.get('teaching'))
-    	if self.request.get('date'):
-    		date = self.request.get('date')
-	    	try:
-	            real_date = datetime.strptime(date,'%m/%d/%Y')
-	        except (ValueError):
-	            real_date = datetime.strptime(date,'%Y-%m-%d')
-	        game.date = real_date
-		game.put()
+        namespace_manager.set_namespace(users.get_current_user().user_id())
+        key_str = self.request.get('key')
+        key = ndb.Key(urlsafe=key_str)
+        game = key.get()
+        if not game:
+            self.response.set_code(500)
+            return
+        if self.request.get('player_faction'):
+            game.player_faction = self.request.get('player_faction')
+        if self.request.get('player_warcaster'):
+            game.player_warcaster = self.request.get('player_warcaster')
+        if self.request.get('opponent_name'):
+            game.opponent_name = self.request.get('opponent_name')
+        if self.request.get('opponent_faction'):
+            game.opponent_faction = self.request.get('opponent_faction')
+        if self.request.get('opponent_warcaster'):
+            game.opponent_warcaster = self.request.get('opponent_warcaster')
+        if self.request.get('result'):
+            game.result = self.request.get('result')
+        if self.request.get('size'):
+            game.size = int(self.request.get('size', 0))
+        if self.request.get('won'):
+            game.won = bool(self.request.get('won'))
+        if self.request.get('draw'):
+            game.draw = bool(self.request.get('draw'))
+        if self.request.get('teaching'):
+            game.teaching = bool(self.request.get('teaching'))
+        if self.request.get('date'):
+            date = self.request.get('date')
+            try:
+                real_date = datetime.strptime(date,'%m/%d/%Y')
+            except (ValueError):
+                real_date = datetime.strptime(date,'%Y-%m-%d')
+            game.date = real_date
+        game.put()
 
     def delete(self):
-    	"""
-    	DELETE an existing Game entity
-    	"""
+        """
+        DELETE an existing Game entity
+        """
         self.record_usage()
-    	namespace_manager.set_namespace(users.get_current_user().user_id())
-    	key_str = self.request.url
-    	key_str = self.request.url[self.request.url.rfind('/')+1:]
-    	key = ndb.Key(urlsafe=key_str)
-    	key.delete()
+        namespace_manager.set_namespace(users.get_current_user().user_id())
+        key_str = self.request.url
+        key_str = self.request.url[self.request.url.rfind('/')+1:]
+        key = ndb.Key(urlsafe=key_str)
+        key.delete()
 
 app = webapp2.WSGIApplication([
     ('/game.*', GameHandler)
