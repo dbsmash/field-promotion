215abb296eeb6fad6c4ec62432312c5ecdeb1211
Colby Natale <dbsmash@gmail.com>
2014-08-08 16:18:10 -0600

    history chart

--- app/js/controllers/analyzeController.js
+++ app/js/controllers/analyzeController.js
@@ -1,4 +1,32 @@
 var AnalyzeController = function($scope, StatsService) {
+	var loadHistoryChart = function(response) {
+		var datasets = [];
+		for (key in response) {
+			if (response.hasOwnProperty(key)) {
+				datasets.push({
+					label: key,
+					strokeColor: StatsService.getColorForFaction(key),
+					pointColor: StatsService.getColorForFaction(key),
+					data: response[key].history
+				});
+			}
+		}
+
+		var data = {
+			labels: ["Last Month", "2 months ago", "3 months ago", "4 months ago", "5 months ago", "6 months ago", "7 months ago", "8 months ago", "9 months ago", "10 months ago", "11 months ago", "12 months ago"],
+			datasets: datasets
+		};
+		var ctx = document.getElementById("myChart3").getContext("2d");
+		var options = {
+			legendTemplate : "<ul><% for (var i=0; i<datasets.length; i++){%><li><span style=\"color:<%=datasets[i].pointColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>",
+			datasetFill: false,
+			bezierCurve: true
+		};
+		var myBarChart = new Chart(ctx).Line(data, options);
+		var leg = myBarChart.generateLegend();
+		$("#legend").html(leg);
+	};
+
 	var loadFactionNumberChart = function(response) {
 		var names = [];
 		var wins = [];
@@ -57,6 +85,7 @@ var AnalyzeController = function($scope, StatsService) {
 	var loadCharts = function(response) {
 		loadFactionNumberChart(response);
 		loadFactionWinChart(response);
+		loadHistoryChart(response);
 	}
 
 	var data = StatsService.getStats($scope, loadCharts);
